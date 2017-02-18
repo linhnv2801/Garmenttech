5b1fbd7f1ebe8e77f25a1b4599bffb45f628b790
thuyictk55 <Onion@Onion-PC>
2017-02-19 00:32:07 +0700

    admin priviledge and user permission

---
+++ advanced/backend/config/main-local.php
@@ -0,0 +1,33 @@
+<?php
+
+$config = [
+        'components' => [
+            'request' => [
+            'enableCookieValidation' => true,
+            'enableCsrfValidation' => true,
+            'cookieValidationKey' => 'xxxxxxx',
+        ],
+            'db' =>[
+                'class' => 'yii\db\Connection',
+                'dsn' => 'mysql:host=127.0.0.1;dbname=demo',
+                'username' => 'root',
+                'password' => '',
+                'charset' => 'utf8',
+            ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
--- advanced/backend/views/user/index.php
+++ advanced/backend/views/user/index.php
@@ -4,27 +4,30 @@ use yii\helpers\Html;
 use yii\grid\GridView;
 
 /* @var $this yii\web\View */
+/* @var $searchModel common\models\AdminSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Users';
+$this->title = 'Admins';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="user-index">
+<div class="admin-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
+    <?php  echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create User', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Create Admin', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
             'id',
+            'username:ntext',
             'email:ntext',
             'password_hash:ntext',
-            'username:ntext',
             'status',
             // 'created_at',
             // 'updated_at',
